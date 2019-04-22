dc83198c757bead2e6a2a5322cc93e7fc5b66050
chenchang <1814625556@qq.com>
2019-04-22 11:36:09 +0800

    weblistener 解析

--- Http/Program.cs
+++ Http/Program.cs
@@ -12,9 +12,21 @@ namespace Http
         {
             //RunMethod();
             //启动http服务器
-            HttpServer.StartServerAsync(new string[] { });
+            //HttpServer.StartServerAsync(new string[] { });
+            args = new[] {"http://localhost:9001/samples"};
+            RunServer(args);
             Console.WriteLine("method is over...");
             Console.ReadKey();
+        }
+
+        static void RunServer(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                //ShowUsage();
+                return;
+            }
+            HttpServer.StartServerAsync(args).Wait();
         }
 
         static async void RunMethod()
